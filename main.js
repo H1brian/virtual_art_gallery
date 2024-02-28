67fbf31a5cb768c1177a5ab246c207aa05818b7d
H1brian <133236039+H1brian@users.noreply.github.com>
2024-02-28 11:54:48 -0500

    Add paintings and controls

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { PointerLockControls, ThreeMFLoader } from 'three-stdlib';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // const width = window.innerWidth, height = window.innerHeight;
 
@@ -35,7 +36,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Scene
 const scene = new THREE.Scene(); // create the scene
-const loader = new THREE.TextureLoader(); // create image loader
+const textureLoader = new THREE.TextureLoader(); // create image loader
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -65,9 +66,9 @@ sunLight.position.y = 15;
 scene.add(sunLight);
 
 // Creating a cube, name is mesh, a combination of geometry (shape) and material(how it looks)
-let geometry = new THREE.BoxGeometry(1, 1, 1); // the shape of the object, parameters are the size of the box
-let material = new THREE.MeshBasicMaterial({color: 'blue'}); // color
-let cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(1, 1, 1); // the shape of the object, parameters are the size of the box
+const material = new THREE.MeshBasicMaterial({color: 'blue'}); // color
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 // Controls
@@ -76,7 +77,7 @@ document.addEventListener('keydown', onkeyDown, false);
 
 // Texture of the floor
 // let floorTexture = new THREE.ImageUtils.loadTexture('img/floor.jpg') // for new version of threejs
-const floorTexture = loader.load('img/floor.jpg');
+const floorTexture = textureLoader.load('src/public/img/floor.jpg');
 
 // Create the floor plane
 const planeGeometry = new THREE.PlaneGeometry(50, 50); // BoxGeometry is the shape of the object // Alternative PlaneBufferGeometry
@@ -94,7 +95,7 @@ scene.add(floorPlane);
 const wallGroup = new THREE.Group();
 scene.add(wallGroup);
 // Create the texture of the wall
-const wallTexture = loader.load('./img/wall.jpg');
+const wallTexture = textureLoader.load('src/public/img/wall.jpg');
 
 // Front wall
 const frontWall = new THREE.Mesh(
@@ -105,7 +106,16 @@ const frontWall = new THREE.Mesh(
 	})
 );
 frontWall.position.z = -20;  // push the frontwall to the rear position
-// wallGroup.add(frontWall);  // add walls to the wallGroup
+
+// Back wall
+const backWall = new THREE.Mesh(
+	new THREE.BoxGeometry(50, 20, 0.001),
+	new THREE.MeshBasicMaterial({
+		// color: "red",
+		map: wallTexture
+	})
+);
+backWall.position.z = 20;  // push the frontwall to the rear position
 
 // Left wall
 const leftWall = new THREE.Mesh(
@@ -114,7 +124,6 @@ const leftWall = new THREE.Mesh(
 );
 leftWall.rotation.y = Math.PI / 2;  // rotate the leftwall 90 degree
 leftWall.position.x = -20;   // move the leftwall to the left
-// wallGroup.add(leftWall);  // add walls to the wallGroup
 
 // Right wall
 const rightWall = new THREE.Mesh(
@@ -124,16 +133,16 @@ const rightWall = new THREE.Mesh(
 rightWall.rotation.y = Math.PI / 2;  // rotate the leftwall 90 degree
 rightWall.position.x = 20;   // move the leftwall to the left
 
-wallGroup.add(frontWall, leftWall, rightWall);  // add walls to the wallGroup
+wallGroup.add(frontWall, backWall, leftWall, rightWall);  // add walls to the wallGroup
 
 // Look through each wall and create the bounding box
 // for (let i = 0; i < wallGroup.children.length; i++) {
 // 	wallGroup.children[i].Box3
 // }
 
-// Create the ceiling
 // Create the ceiling texture
-const ceilingTexture = loader.load('img/ceiling.jpg');
+const ceilingTexture = textureLoader.load('src/public/img/ceiling.jpg');
+// Create the ceiling
 const ceilingGeometry = new THREE.PlaneGeometry(50, 50); // BoxGeometry is the shape of the objects
 const ceilingMaterial = new THREE.MeshBasicMaterial({
 	// color: "yellow",
@@ -146,25 +155,86 @@ ceilingPlane.rotation.x = Math.PI / 2;  // rotate the ceiling 90 degree
 ceilingPlane.position.y = 10;
 scene.add(ceilingPlane);
 
+// Create paintings
+
+function createPainting(imageURL, width, height, position) {  // A fucntion to load a image texture and create a painting mesh
+	const paintingTexture = textureLoader.load(imageURL);   // Load image texture
+	const paintingGeometry = new THREE.PlaneGeometry(width, height); // Create geometry
+	const paintingMaterial = new THREE.MeshBasicMaterial({   //Create material
+		// color: "red",
+		map: paintingTexture,
+	});
+	const painting = new THREE.Mesh(paintingGeometry, paintingMaterial);
+	painting.position.set(position.x, position.y, position.z);
+	// painting.position.z = -15;
+	// painting.rotation.y = Math.PI /2;
+	// painting.position.x = -19;
+	return painting;
+};
+
+const painting1 = createPainting(
+	"./src/public/paintings/zhangdaqian1.jpg", 
+	10, 
+	5, 
+	new THREE.Vector3(-10, 3, -19.99)
+);
+const painting2 = createPainting(
+	"./src/public/paintings/zhangdaqian2.jpg", 
+	10, 
+	5, 
+	new THREE.Vector3(10, 3, -19.99)
+);
+
+scene.add(painting1, painting2);
+
+// Controls
+const controls = new PointerLockControls(camera, document.body);
+
+// Lock the pointer (controls are activated)
+// And hide the menu when the experience starts
+function startExperience(){
+	// Lock the pointer
+	controls.lock();
+	// Hide the menu
+	hideMenu();
+};
+const startButton = document.getElementById("start_button");
+startButton.addEventListener("click", startExperience);
+
+// Hide the menu
+function hideMenu(){
+	const menu = document.getElementById('menu');
+	menu.style.display = "none";
+};
+
+// Show the menu
+function showMenu(){
+	const menu = document.getElementById('menu');
+	menu.style.display = "block";
+};
+
+// Press "ESC" to exit and show the menu
+controls.addEventListener('unlock', showMenu);
+
 // Function when a key is pressed, execute this function
 function onkeyDown(event) {
 	let keycode = event.which;
 
 	//right arrow key or letter d
-	if (keycode === 39) {
-		camera.translateX(0.05);
+	if (keycode === 39 || keycode === 68) {
+		controls.moveRight(0.2);
 	}
 	// left arrow key or letter a
-	else if (keycode === 37) {
-		camera.translateX(-0.05)
+	else if (keycode === 37 || keycode === 65) {
+		controls.moveRight(-0.2);
 	}
 	// up arrow key or letter w
-	else if (keycode === 38) {
-		camera.translateY(0.05)
+	else if (keycode === 38 || keycode === 87) {
+		controls.moveForward(0.2);
 	}
 	// down arrow key or letter s
-	else if (keycode === 40) {
-		camera.translateY(-0.05)
+	else if (keycode === 40 || keycode === 83) {
+		controls.moveForward(-0.2);
 	}
 };
 
