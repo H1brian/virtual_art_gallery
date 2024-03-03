dff3404f7d384c99bc75fb45274b09971361bc41
H1brian <133236039+H1brian@users.noreply.github.com>
2024-03-02 21:48:19 -0500

    Add feature1

--- main.js
+++ main.js
@@ -1,38 +1,7 @@
 import * as THREE from 'three';
 import { PointerLockControls, ThreeMFLoader } from 'three-stdlib';
 // import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-// const width = window.innerWidth, height = window.innerHeight;
-
-// // init
-
-// const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-// camera.position.z = 1;
-
-// const scene = new THREE.Scene();
-
-// const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-// const material = new THREE.MeshNormalMaterial();
-
-// const mesh = new THREE.Mesh( geometry, material );
-// scene.add( mesh );
-
-// const renderer = new THREE.WebGLRenderer( { antialias: true } );
-// renderer.setSize( width, height );
-// renderer.setAnimationLoop( animation );
-// document.body.appendChild( renderer.domElement );
-
-// // animation
-
-// function animation( time ) {
-
-// 	mesh.rotation.x = time / 2000;
-// 	mesh.rotation.y = time / 1000;
-
-// 	renderer.render( scene, camera );
-
-// }
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Scene
 const scene = new THREE.Scene(); // create the scene
@@ -70,10 +39,6 @@ const geometry = new THREE.BoxGeometry(1, 1, 1); // the shape of the object, par
 const material = new THREE.MeshBasicMaterial({color: 'blue'}); // color
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
-// Controls
-// Event listener when user presee the keys
-// document.addEventListener('keydown', onkeyDown, false);
 
 // Texture of the floor
 // let floorTexture = new THREE.ImageUtils.loadTexture('img/floor.jpg') // for new version of threejs
